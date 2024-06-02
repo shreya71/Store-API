8b4e0b4c192e32e8d943587a10427666dc28b2ea
Shreya <shreyasingh070801@gmail.com>
2024-06-02 17:24:55 +0530

    Setup boilerplate and middleware

---
+++ app.js
@@ -0,0 +1,39 @@
+require('dotenv').config()
+
+//async errors
+
+const express = require('express');
+const app = express();
+
+const notFoundMidddleware = require('./middleware/not-found')
+const errorMidddleware = require('./middleware/error-handler')
+
+//middleware
+app.use(express.json())
+
+//routes
+
+app.get('/', (req,res)=>{
+    res.send('<h1>Store API</h1><a href="/api/v1/products">products route</a>')
+})
+
+//products routes
+
+
+
+
+app.use(notFoundMidddleware)
+app.use(errorMidddleware)
+
+const port = process.env.PORT || 3000
+
+const start = async () =>{
+    try{
+        //connectDB
+        app.listen(port, console.log(`Server is listening port ${port}...`))
+    } catch(error) {
+        console.log(error)
+    }
+}
+
+start()
